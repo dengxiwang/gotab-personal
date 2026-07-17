1e2f3ea30f59691c41eb53df6f98055f05c87f51
doxwant <41525895+dengxiwang@users.noreply.github.com>
2026-07-18 02:19:35 +0800

    修复天气、热点小组件编辑后请求无法正确及时更新的问题

---
+++ web/assets/index-Dboa6bsY.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-D8nDyRPw.js";import{a as t,o as n}from"./themeModeProvider-Cj7F9RjW.js";import{t as r}from"./main-Din-9Fvw.js";var i=e();t({sortable:!0}),n((0,i.jsx)(r,{}));
