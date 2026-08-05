1f328463326f9f4d3188f9b08c1979e205db21c9
doxwant <41525895+dengxiwang@users.noreply.github.com>
2026-08-05 10:53:27 +0800

    界面缩放支持其余区域等比缩放

---
+++ web/assets/index-DnVqp8VC.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-B5yqYJvp.js";import{a as t,o as n}from"./themeModeProvider-8CgDdzdb.js";import{t as r}from"./main-wRfUVh2A.js";var i=e();t({sortable:!0}),n((0,i.jsx)(r,{}));
