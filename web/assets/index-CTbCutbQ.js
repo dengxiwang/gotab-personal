3b0dc578cc8775f085a4b31f14b13802095f4ce9
doxwant <41525895+dengxiwang@users.noreply.github.com>
2026-08-06 19:27:46 +0800

    自由拖拽布局新增坐标原点设置

---
+++ web/assets/index-CTbCutbQ.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-B5yqYJvp.js";import{a as t,o as n}from"./themeModeProvider-B9vdLpNs.js";import{t as r}from"./main-Cggl5EqA.js";var i=e();t({sortable:!0}),n((0,i.jsx)(r,{}));
--- web/assets/newtab-gnCs7crk.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./jsx-runtime-B5yqYJvp.js";import{a as t,o as n}from"./themeModeProvider-8CgDdzdb.js";import{t as r}from"./main-wRfUVh2A.js";import{t as i}from"./myErrorPage-bhVpOMCE.js";var a=e();t({sortable:!0}),n(window.location.protocol.includes(`http`)?(0,a.jsx)(i,{}):(0,a.jsx)(r,{type:`ext`}));
