592cc05801b7c98307148f2cd1af6966d11458eb
doxwant <41525895+dengxiwang@users.noreply.github.com>
2026-09-11 12:20:36 +0800

    修复拖拽图标卡住、名称宽度异常问题，优化设置项搜索以及文件夹动画效果

---
+++ web/assets/newtab-BHaHmtFK.js
@@ -0,0 +1,1 @@
+import{ds as e,fs as t,t as n,us as r}from"./myErrorPage-_Ogo2Mf1.js";import{t as i}from"./jsx-runtime-DpJIb57s.js";import{t as a}from"./main-XvYRMPOr.js";var o=i();r(),e().catch(e=>{console.error(`Sortable 初始化失败`,e)}).then(()=>t(window.location.protocol.includes(`http`)?(0,o.jsx)(n,{}):(0,o.jsx)(a,{type:`ext`})));
